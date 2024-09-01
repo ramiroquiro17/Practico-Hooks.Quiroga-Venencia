d1e75521b67c30468976a6982dd4f2c1cb9e2072
MatiasJVenencia <130709845+MatiasJVenencia@users.noreply.github.com>
2024-09-01 04:15:16 -0300

    Add files via upload

---
+++ pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RecipeList from '../components/recipes/RecipeList';
+
+function Home() {
+
+  return (
+    <><div>
+      <h1>Mis Recetas</h1>
+      <RecipeList />
+    </div></>
+  );
+}
+
+export default Home;
